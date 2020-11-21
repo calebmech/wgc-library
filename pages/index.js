7bb0a58cecb750096991f76582265f4e455f86ea
Caleb Mech <cmech1@gmail.com>
2020-11-21 13:09:18 -0500

    Initial commit for GitHub

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,62 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import isbnData from "../public/isbn.json";
 
-export default function Home() {
+// const BASE_URL = "https://www.googleapis.com/books/v1/"
+const BASE_URL = "https://openlibrary.org/"
+
+export default function Home({ isbns, books, }) {
+  // console.log(isbns)
+  // console.log(books)
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div>
+      <h1>Hello world!</h1>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <ul>
+        {
+        books.map((book, i) => {
+          let fullTitle = book.title
+          if (book.subtitle) {
+            fullTitle += ": " + book.subtitle
+          }
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+          return (
+            <li key={i}>{fullTitle}</li>
+          );
+        })
+        }
+      </ul>
     </div>
   )
+}
+
+export async function getStaticProps(context) {
+  const isbns = isbnData;
+
+  const responses = await Promise.all(isbns.map(isbn => {
+    // return fetch(BASE_URL + `volumes?q=isbn:` + isbn).then(r => r.json())
+    return fetch(BASE_URL + 'isbn/' + isbn + '.json').then(r => r.json()).catch(() => ({ totalItems: 0 }))
+  }))
+
+
+  console.log(responses);
+
+  // const books = responses
+  //   .filter(response => response.totalItems > 0)
+  //   .map(response => response.items[0].volumeInfo)
+
+  const books = responses
+    
+  // const leftOverBooks = responses
+  //   .filter(response => response.totalItems === 0)
+
+  // const leftOverResponses = await leftOverBooks.all(isbns.map(isbn => {
+  //   return fetch(BASE_URL + `volumes?q=isbn:` + isbn).then(r => r.json())
+  // }))
+
+  return {
+    props: {
+      isbns,
+      books,
+    }
+  }
 }
