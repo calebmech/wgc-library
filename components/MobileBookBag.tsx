9d0e6f97557e3d46bfbe0f85c310080be8d527d6
Caleb Mech <cmech1@gmail.com>
2021-02-05 21:43:52 -0500

    Dark mode fix

--- components/MobileBookBag.tsx
+++ components/MobileBookBag.tsx
@@ -6,11 +6,11 @@ import {
   ModalCloseButton,
   ModalBody,
   ModalFooter,
+  useColorModeValue,
 } from '@chakra-ui/react';
 import React from 'react';
 import BookBag from './BookBag';
 import BookBagForm from './BookBagForm';
-import styles from './MobileBookBag.module.css';
 
 const MobileBookBag = ({ isOpen, onClose }: { isOpen: boolean; onClose: () => void }) => (
   <Modal isOpen={isOpen} onClose={onClose}>
@@ -22,7 +22,22 @@ const MobileBookBag = ({ isOpen, onClose }: { isOpen: boolean; onClose: () => vo
         <BookBag />
       </ModalBody>
 
-      <ModalFooter className={styles.footer}>
+      <ModalFooter
+        position="relative"
+        _before={{
+          display: 'block',
+          content: '""',
+          width: '100%',
+          height: 30,
+          position: 'absolute',
+          top: -30,
+          left: 0,
+          backgroundImage: `linear-gradient(to top, ${useColorModeValue(
+            'rgba(255, 255, 255, 1)',
+            'rgba(45, 55, 72, 1)'
+          )}, ${useColorModeValue('rgba(255, 255, 255, 0)', 'rgba(45, 55, 72, 0)')} )`,
+        }}
+      >
         <BookBagForm onSubmit={onClose} />
       </ModalFooter>
     </ModalContent>
