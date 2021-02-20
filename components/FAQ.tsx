960a79d1b6150981baad1dff62bd8c78533f42e8
Caleb Mech <cmech1@gmail.com>
2021-02-20 14:30:35 -0500

    Updated FAQ

--- components/FAQ.tsx
+++ components/FAQ.tsx
@@ -28,16 +28,22 @@ const faqItems: FAQItem[] = [
   {
     heading: 'When will my library items be ready?',
     body:
-      'Requested items will be prepared for pickup once a week (usually on Saturdays). You will receive an email to let you know when your items are ready.',
+      'Requested items will be prepared for pickup once a week (usually on Saturdays). You will receive an email to let you know when your items are ready. If you have not received a response within a week, please check your Spam or Junk email folder.',
   },
   {
     heading: 'How will I get my library items?',
     body:
-      '<p>Your items will be placed in the church foyer, either in your mailbox or in a labelled bag. You will need to come into the church during office hours to pick them up.</p><p>During times when we are able to meet in person for worship services, your items will generally be left on your seat in the sanctuary.</p>',
+      '<p>Your items will be placed in a labelled bag in the mailbox carousel in the church foyer, either in your mailbox or in one of the larger slots on the bottom. You will need to come into the church during office hours to pick them up.</p><br /><p>During times when we are able to meet in person for worship services, your items may also be left on your seat in the sanctuary.</p>',
+  },
+  {
+    heading: 'How long can I keep my library items?',
+    body:
+      'Keep your items as long as you are making use of them. Just so we don’t lose track, we will send you a reminder if your items have not been returned after one month, and every 3 weeks after that.',
   },
   {
     heading: 'How will I return library items?',
-    body: 'There will be a labelled cart on the lower level of the church foyer for returns.',
+    body:
+      'Place them in the labelled bin on the lower level of the church foyer. When we are meeting in person, this will be to your left just as you enter them main front doors.',
   },
   {
     heading: 'What if I can’t find the item I’m looking for?',
@@ -45,14 +51,9 @@ const faqItems: FAQItem[] = [
       'There is a place for ‘Additional information’ at the bottom of the ‘Book bag’ section. Please let us know what you are looking for as part of your order or send an email to <a href="mailto:library@winonagospelchurch.org">library@winonagospelchurch.org</a>, and we’ll get back to you.',
   },
   {
-    heading: 'How long can I keep my library items?',
-    body:
-      'Keep your items as long as you are making use of them. Just so we don’t lose track, we will plan to send you a reminder if your items have not been returned after one month.',
-  },
-  {
     heading: 'What precautions are being taken related to the pandemic?',
     body:
-      '<p>When preparing items for pickup, the volunteer will be wearing a mask and cleaning their hands regularly.</p><p>Returned books will be ‘quarantined’ for at least a week before being recirculated or reshelved. Returned CD and DVD cases will also be cleaned with disinfectant wipes before being recirculated or reshelved.</p>',
+      '<p>When preparing items for pickup, the volunteer will be wearing a mask and cleaning their hands regularly.</p><p>Returned items will be ‘quarantined’ for about a week (at least 3 days) before being recirculated or reshelved.</p>',
   },
   {
     heading: 'What if I have a question not on this list?',
@@ -68,11 +69,6 @@ const FAQ = ({ isOpen, onClose }: { isOpen: boolean; onClose: () => void }) => (
       <ModalHeader>Frequently Asked Questions</ModalHeader>
       <ModalCloseButton />
       <ModalBody>
-        <Text mb="4" textAlign="center" color="red.600">
-          ALL OF THE FOLLOWING RESPONSES ARE JUST INITIAL SUGGESTIONS AND NEED TO BE CLARIFIED/CONFIRMED AND SHOULD BE
-          REVISED BEFORE THE WEBSITE GOES LIVE.
-        </Text>
-
         <UnorderedList>
           {faqItems.map(({ heading, body }) => (
             <ListItem mb={4} key={heading}>
