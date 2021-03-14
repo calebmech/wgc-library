f15ce8c023fcf00b99fb99a68df76b392b9f3667
Caleb Mech <cmech1@gmail.com>
2021-03-13 22:24:19 -0500

    Fixed year for Safari

--- components/Search/SearchResultDescription.tsx
+++ components/Search/SearchResultDescription.tsx
@@ -70,7 +70,7 @@ export default function SearchResultDescription({
                   <TagLeftIcon>
                     <CalendarIconSm />
                   </TagLeftIcon>
-                  {new Date(item.releaseDate).getFullYear()}
+                  {item.releaseDate.slice(0, 4)}
                 </Tag>
               </Tooltip>
             </WrapItem>
