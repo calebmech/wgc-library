998487a295435113ab9f12648019be2efb1e1052
Caleb Mech <cmech1@gmail.com>
2024-03-23 11:34:25 -0400

    Updated audience labels

--- components/Search/GroupSelector.tsx
+++ components/Search/GroupSelector.tsx
@@ -10,15 +10,25 @@ export default function GroupSelector() {
     <Select
       background={useColorModeValue('white', 'gray.700')}
       value={group ?? ''}
-      onChange={(event) => setGroup(event.target.value ? (event.target.value as Group) : undefined)}
+      onChange={(event) =>
+        setGroup(event.target.value ? (event.target.value as Group) : undefined)
+      }
       aria-label="Format"
     >
       <option value="">All audiences</option>
       {Object.values(Group).map((group) => (
         <option key={group} value={group}>
-          {group}
+          {groupToLabel[group]}
         </option>
       ))}
     </Select>
   );
 }
+
+const groupToLabel: Record<Group, string> = {
+  Y: 'Youth',
+  Family: 'Family',
+  Jr: 'Junior',
+  CH: 'Children',
+  "Gino's Pick": "Gino's Pick",
+};
